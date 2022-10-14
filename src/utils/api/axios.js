5dbba7884c0fe42556acce32023bdbd9017fbf60
Benji4C <thegreatben27@gmail.com>
2022-10-14 20:33:07 +0100

    changed base url

--- src/utils/api/axios.js
+++ src/utils/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = "http://localhost:4000";
+const baseUrl = "https://change-airtime-to-cash.herokuapp.com/";
 
 export const apiGet = (path, conf = {}, auth = true) => {
   const config = {
